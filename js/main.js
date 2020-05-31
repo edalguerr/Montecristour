f5fe844444e61249dc1354766a841bbaa594f746
Edalguerr <eguerreroa1@gmail.com>
2020-05-30 20:12:29 -0500

    Ajustes al sitio para que el navbar solo se despliegue en pantallas de menor ancho al x-large, y ajusté el overflow-x del body para evitar la aparicion de una barra de dezplazamiento en la parte inferior de la ventana

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-var navbar = document.getElementById("navbar");
-var display = false;
+var screen = {
+	Xlarge: 1200
+};
 
-navbar.addEventListener('click', () => {
-	
-	navbar.style.height = ((display)? "40px":"222px");
-	display = !display;
-	
-});
+if(window.innerWidth < screen.Xlarge){
+	initLisenerForNavItemsResponsive();
+}
+
+function initLisenerForNavItemsResponsive() {
+	var navbar = document.getElementById("navbar");
+	var display = false;
+
+	navbar.addEventListener('click', () => {
+
+		navbar.style.height = ((display) ? "40px" : "222px");
+		display = !display;
+
+	});
+}
